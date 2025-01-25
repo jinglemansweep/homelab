b5fbfab345e89204dfde8bbf0934da4a2c70b937
Louis King <jinglemansweep@gmail.com>
2025-01-25 15:55:13 +0000

    Updates

--- ansible/docker-entrypoint.sh
+++ ansible/docker-entrypoint.sh
@@ -7,16 +7,21 @@ echo "Repo:      ${ANSIBLE_PULL_REPO}"
 echo "Playbook:  ${ANSIBLE_PULL_PLAYBOOK}"
 echo
 
-ansible-pull -K -v \
+ansible-pull -v \
   -d ${ANSIBLE_PULL_HOME} \
   -U ${ANSIBLE_PULL_REPO} \
   ${ANSIBLE_PULL_PLAYBOOK} \
-  -i localhost, \
+  -i "localhost," \
   --extra-vars pull_home=${ANSIBLE_PULL_HOME}
+
+# exit 0
 
 echo
 echo "Debug"
 echo
 echo "/opt/ansible-pull:"
 ls -l /opt/ansible-pull
+echo
+echo "/opt/ansible-pull/ansible:"
+ls -l /opt/ansible-pull/ansible
 echo
