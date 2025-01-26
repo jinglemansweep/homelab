63c80b9cde60c36c55eaa31c8dec3bcc9f75b26b
Louis King <jinglemansweep@gmail.com>
2025-01-26 20:37:51 +0000

    Updates

--- docker/compose/scripts/compose.sh
+++ docker/compose/scripts/compose.sh
@@ -2,3 +2,15 @@
 
 echo "COMPOSE"
 echo
+
+declare -r service_list="$(ls ./services)"
+declare compose_file=""
+
+for service in ${service_list}
+do
+  compose_file="${compose_file}./services/${service}/docker-compose.yml:"
+done
+
+export COMPOSE_FILE="${compose_file%:}"
+
+echo $COMPOSE_FILE
