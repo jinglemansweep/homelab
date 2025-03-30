ff1f6f7298260ba2c677fa43da06c47083827d72
Louis King <jinglemansweep@gmail.com>
2025-03-30 16:15:19 +0100

    Updated env script

--- env.sh
+++ env.sh
@@ -4,6 +4,16 @@
 
 if [ ! -z "${INFISICAL_PROJECT_ID}" -a ! -z "${INFISICAL_UNIVERSAL_AUTH_CLIENT_ID}" -a ! -z "${INFISICAL_UNIVERSAL_AUTH_CLIENT_SECRET}" ]; then
     export INFISICAL_TOKEN="$(infisical login --method=universal-auth --client-id=${INFISICAL_UNIVERSAL_AUTH_CLIENT_ID} --client-secret=${INFISICAL_UNIVERSAL_AUTH_CLIENT_SECRET} --silent --plain)"
-    export TF_VAR_infisical_workspace_id="${INFISICAL_PROJECT_ID}"
-    alias infisicalw="infisical --projectId=${INFISICAL_PROJECT_ID} --token=${INFISICAL_TOKEN}"
+else
+    echo "Missing required environment variables for Infisical login"
+    exit 1
 fi
+
+# Infisical
+alias lab_infisical="infisical --projectId=${INFISICAL_PROJECT_ID} --token=${INFISICAL_TOKEN}"
+
+# Terraform
+export TF_VAR_infisical_workspace_id="${INFISICAL_PROJECT_ID}"
+function lab_terraform() {
+  infisicalw run -- terraform -chdir=./terraform $1 -var-file=../secrets.tfvars
+}
